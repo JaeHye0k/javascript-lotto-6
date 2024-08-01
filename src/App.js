b868127a5e0df33d78bc21e10c5783e3340e75bf
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-08-01 12:52:03 +0900

    feat(issueLottos, getLotto): 로또 발행 기능 추가
    - issueLottos: 구입 금액을 인수로 받아 금액에 맞게 로또를 발행
    - getLotto: 로또 하나를 발행

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
+	startNum = 1;
+	endNum = 45;
+	numCount = 6;
+
+	constructor() {
+		this.issuedLottos = [];
+		this.cost = 0;
+		this.winningNumbers;
+		this.bonusNumbers;
+	}
+
 	async play() {
 		const inputGenerator = this.input();
-		const cost = (await inputGenerator.next()).value;
-		const winningNumbers = (await inputGenerator.next()).value;
-		const bonusNumbers = (await inputGenerator.next()).value;
-		console.log(cost, winningNumbers, bonusNumbers);
+		// const outputGenerator = this.output();
+		this.cost = (await inputGenerator.next()).value;
+		this.winningNumbers = (await inputGenerator.next()).value;
+		this.bonusNumbers = (await inputGenerator.next()).value;
+		this.issueLottos(+this.cost);
+		// outputGenerator.next();
 	}
 
 	async *input() {
 		yield await Console.readLineAsync("구입금액을 입력해 주세요.\n");
 		yield await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
 		yield await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+	}
+
+	// *output() {
+	// 	yield Console.print(`${this.issuedLottos.length}개를 구매했습니다. `);
+	// }
+
+	issueLottos(cost) {
+		const count = cost / 1000;
+		for (let i = 0; i < count; i++) {
+			this.issuedLottos.push(this.getLotto());
+		}
+	}
+
+	getLotto() {
+		const randomNums = Random.pickUniqueNumbersInRange(
+			this.startNum,
+			this.endNum,
+			this.numCount
+		);
+		randomNums.sort();
+		return new Lotto(randomNums);
 	}
 }
 
