b7958f14d1c961b449d1b7468682cb7ce9012ee9
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-08-01 11:04:16 +0900

    feat(input):  입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+	async play() {
+		const inputGenerator = this.input();
+		const cost = (await inputGenerator.next()).value;
+		const winningNumbers = (await inputGenerator.next()).value;
+		const bonusNumbers = (await inputGenerator.next()).value;
+		console.log(cost, winningNumbers, bonusNumbers);
+	}
+
+	async *input() {
+		yield await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+		yield await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+		yield await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+	}
 }
 
 export default App;
