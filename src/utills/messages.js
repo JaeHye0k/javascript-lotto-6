89821eee34fa96468692ad0fa68a584187df3b69
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-08-08 16:54:43 +0900

    feat(messages.js): 입출력 메세지 모듈 추가

---
+++ src/utills/messages.js
@@ -0,0 +1,10 @@
+export const MESSAGES = {
+	INPUT: {
+		COST: "구입금액을 입력해 주세요.\n",
+		WINNING_NUM: "\n당첨 번호를 입력해 주세요.\n",
+		BONUS_NUM: "\n보너스 번호를 입력해 주세요.\n",
+	},
+	OUTPUT: {
+		WINNING_RESULT: "\n당첨 통계\n---\n",
+	},
+};
