a2fa6319fd6802ae232a1a73fcb5e28c866d240e
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-08-08 15:44:35 +0900

    chore(Lotto): 주석(JSDoc) 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,11 +1,19 @@
 class Lotto {
 	#numbers;
 
+	/**
+	 * 6자리 숫자를 갖는 로또를 생성한다.
+	 * @param {Number[]} numbers
+	 */
 	constructor(numbers) {
 		this.#validate(numbers);
 		this.#numbers = numbers;
 	}
 
+	/**
+	 * 로또 번호의 유효성을 검사한다.
+	 * @param {Number[]} numbers
+	 */
 	#validate(numbers) {
 		if (numbers.length !== 6) {
 			throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
@@ -14,21 +22,27 @@ class Lotto {
 
 	// TODO: 추가 기능 구현
 
+	/**
+	 *	인수로 전달된 로또가 몇 등인지 반환한다.
+	 * @param {Lotto} lotto
+	 * @param {Number[]} winningNumbers
+	 * @param {Number[]} bonusNumbers
+	 * @returns {Number}
+	 */
 	static compareNumbers(lotto, winningNumbers, bonusNumbers) {
 		const numbers = lotto.getNumbers();
 		const winningCount = Lotto.getSameNumberCount(numbers, winningNumbers);
 		const bonusCount = Lotto.getSameNumberCount(numbers, bonusNumbers);
 		const rank = Lotto.getRank(winningCount, bonusCount);
-		// console.log("로또 번호: ", numbers);
-		// console.log("당첨 번호: ", winningNumbers);
-		// console.log("당첨 개수: ", winningCount);
-		// console.log("보너스 번호: ", bonusNumbers);
-		// console.log("보너스 개수: ", bonusCount);
-		// console.log(rank + "등");
 		return rank;
 	}
 
-	// 동일한 번호의 개수를 반환
+	/**
+	 * 동일한 번호의 개수를 반환한다.
+	 * @param {Number[]} base
+	 * @param {Number[]} compared
+	 * @returns {Number}
+	 */
 	static getSameNumberCount(base, compared) {
 		let includeCount = 0;
 		for (let i = 0; i < compared.length; i++) {
@@ -37,7 +51,12 @@ class Lotto {
 		return includeCount;
 	}
 
-	// 등수 구하기
+	/**
+	 * 등수를 반환한다.
+	 * @param {Number} winningCount 당첨 번호와 일치하는 번호의 개수
+	 * @param {Number} bonusCount 보너스 번호와 일치하는 번호의 개수 (1또는 0)
+	 * @returns {Number}
+	 */
 	static getRank(winningCount, bonusCount) {
 		if (winningCount < 3) return 0; // 꽝
 		if (winningCount === 3) return 5; // 5등
@@ -49,6 +68,10 @@ class Lotto {
 		if (winningCount === 6) return 1; // 1등
 	}
 
+	/**
+	 * 로또의 6자리 번호를 반환한다.
+	 * @returns {Number}
+	 */
 	getNumbers() {
 		return this.#numbers;
 	}
