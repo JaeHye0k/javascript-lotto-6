473886a1172e006f5189bc753679253a7073465e
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-08-11 12:54:29 +0900

    fix(Lotto): 테스트 코드에 맞게 유효성 검사 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { validator } from "./utills/validator.js";
+
 class Lotto {
 	#numbers;
 
@@ -15,9 +17,7 @@ class Lotto {
 	 * @param {Number[]} numbers
 	 */
 	#validate(numbers) {
-		if (numbers.length !== 6) {
-			throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-		}
+		validator.checkLottoNumber(numbers);
 	}
 
 	// TODO: 추가 기능 구현
