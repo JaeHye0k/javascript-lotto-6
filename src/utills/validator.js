03d03dec77e581bd46c9d83456975bc5377c56c0
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-08-12 19:24:28 +0900

    refactor(validator): 함수를 작게 분리

--- src/utills/validator.js
+++ src/utills/validator.js
@@ -1,31 +1,35 @@
 import { MESSAGES } from "../constant/messages.js";
 
-/** @type {(cost:Number)=>undefined} 구입 금액에 대한 유효성 검사를 수행한다. */
-function checkCost(cost) {
-	if (isNaN(cost)) throw new Error(MESSAGES.ERROR.NAN);
-	if (cost % 1000 !== 0) throw new Error(MESSAGES.ERROR.NOT_DIVIDED);
+function isNotSixDigit(numbers) {
+	if (numbers.length !== 6) throw new Error(MESSAGES.ERROR.NOT_SIX_DIGIT);
 }
 
-/** @type {(winningNumber: Number[])=>undefined} 당첨 번호에 대한 유효성 검사를 수행한다.*/
-function checkLottoNumber(winningNumber) {
-	if (winningNumber.length !== 6) throw new Error(MESSAGES.ERROR.NOT_SIX_DIGIT);
-	if (new Set(winningNumber).size !== 6) throw new Error(MESSAGES.ERROR.DUPLICATION);
-	for (const num of winningNumber) {
-		if (isNaN(num)) throw new Error(MESSAGES.ERROR.NAN);
-		if (num < 1 || num > 45) throw new Error(MESSAGES.ERROR.OUT_OF_RANGE);
-	}
+function isNotOneDigit(numbers) {
+	if (numbers.length !== 1) throw new Error(MESSAGES.ERROR.NOT_ONE_DIGIT);
+}
+function isIncludeDuplicationNumber(numbers, originLenth) {
+	if (new Set(numbers).size !== originLenth) throw new Error(MESSAGES.ERROR.DUPLICATION);
+}
+function isIncludeNaN(numbers) {
+	if (numbers.some((num) => typeof num !== "number" || isNaN(num)))
+		throw new Error(MESSAGES.ERROR.NAN);
+}
+function isIncludeOutOfRangeNumber(numbers) {
+	if (numbers.some((num) => num < 1 || num > 45)) throw new Error(MESSAGES.ERROR.OUT_OF_RANGE);
+}
+function isNotDivieded(cost) {
+	if (cost === 0 || cost % 1000 !== 0) throw new Error(MESSAGES.ERROR.NOT_DIVIDED);
+}
+function isNaNCost(cost) {
+	if (typeof cost !== "number" || isNaN(cost)) throw new Error(MESSAGES.ERROR.NAN);
 }
 
-/** @type {(bonusNumber: Number[])=>undefined} 보너스 번호에 대한 유효성 검사를 수행한다.*/
-function checkBonusNumber(bonusNumber) {
-	if (bonusNumber.length !== 1) throw new Error(MESSAGES.ERROR.NOT_ONE_DIGIT);
-	if (isNaN(bonusNumber)) throw new Error(MESSAGES.ERROR.NAN);
-	if (bonusNumber < 1 || bonusNumber > 45) throw new Error(MESSAGES.ERROR.OUT_OF_RANGE);
-}
-
-/** @type {(numbers: Number[])=>undefined} 보너스 번호와 당첨 번호를 합쳐서 중복된 번호가 있는지 검사한다.*/
-function checkDuplication(numbers) {
-	if (new Set(numbers).size !== 7) throw new Error(MESSAGES.ERROR.DUPLICATION);
-}
-
-export const validator = { checkCost, checkBonusNumber, checkLottoNumber, checkDuplication };
+export const validator = {
+	isNotSixDigit,
+	isNotOneDigit,
+	isIncludeDuplicationNumber,
+	isIncludeNaN,
+	isIncludeOutOfRangeNumber,
+	isNotDivieded,
+	isNaNCost,
+};
